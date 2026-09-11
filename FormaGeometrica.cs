60a673c24ba63ad550ad7c9412a20375b5fe5b17
iza0914 <izabelli.izidoro@edu.senai.br>
2026-09-11 12:07:49 -0300

    Heranca Exercício

---
+++ FormaGeometrica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca
+{
+    public abstract class FormaGeometrica
+
+    {
+
+        public abstract double CalcularArea();
+
+    }
+
+}
